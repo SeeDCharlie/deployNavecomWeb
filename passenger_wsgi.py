ce493010e391bad4dc393e4844f8555689dd322e
SeeDCharlie <hellocarloscastillo@gmail.com>
2021-03-15 11:06:04 -0500

    asd

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,19 +1,17 @@
 import sys, os
 
-INTERP = "/home/navecomingenieri/venvNavecom/bin/python3.9"
+INTERP = "/home/seedch/venvSeedCorp/bin/python3.9"
 #INTERP is present twice so that the new python interpreter 
 #knows the actual executable path 
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 cwd = os.getcwd()
 sys.path.append(cwd)
-sys.path.append(cwd + '/navecomWeb')  #You must add your project here
+sys.path.append(cwd + '/deployNavecomWeb')  #You must add your project here
 
-sys.path.insert(0,'/home/navecomingenieri/venvNavecom/bin')
-sys.path.insert(0,'/home/navecomingenieri/venvNavecom/lib/python3.9/site-packages')
+sys.path.insert(0,'/home/seedch/venvSeedCorp/bin')
+sys.path.insert(0,'/home/seedch/venvSeedCorp/lib/python3.9/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "navecomWeb.settings"
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-
